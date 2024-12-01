6d03cf2cb559537a3c676b06f66e0eb14ffd1783
rythmn1111 <rythmnmagnani0@gmail.com>
2024-12-01 16:54:44 +0530

    wqdwqd

--- src/components/cali/cali.tsx
+++ src/components/cali/cali.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
